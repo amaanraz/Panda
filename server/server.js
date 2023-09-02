84c094409cec0e4acb71fe8fbb5fa1725fb0f8d5
Amaan <amaanraz115@gmail.com>
2023-09-02 12:21:42 -0700

    Quiz Server Functionality

--- server/server.js
+++ server/server.js
@@ -21,11 +21,15 @@ const io = new Server(server, {
 
 const games = {};
 const activePlayers = {};
+let userPlayerPoints = {};
+var currentQIndex = -1;
+// socket id: [name, score, gamepin]
 // user connection
 io.on("connection", (socket) => {
   // host game
   socket.on("host-game", (hostData) => {
     const gamepin = generateGamePin();
+    currentQIndex = -1;
     // TODO: add checking if game exists via game pin
     games[gamepin] = {
       host: { name: hostData.name, socketId: socket.id },
@@ -50,6 +54,8 @@ io.on("connection", (socket) => {
         gamepin: data.gamepin,
       };
 
+      userPlayerPoints[socket.id] = [data.name,0,data.gamepin];
+
       // for the join front end to get a message
       socket.emit('waiting', {gamepin});
     } else {
@@ -64,6 +70,36 @@ io.on("connection", (socket) => {
     if(games[gamepin]){
       const roomName = JSON.stringify(gamepin);
       io.to(roomName).emit('confirm-start');
+    }
+  })
+
+  // recive question request
+  socket.on('question', (data) => {
+    var question = "";
+    currentQIndex++;
+    const roomName = JSON.stringify(data.gamepin);
+    
+    if(currentQIndex >= questions.length){
+      question = "REACHED END LOSER";
+      // sort userPoints, emit an end game event passing top players.
+      const sortedValues = Object.values(userPlayerPoints).sort(([, a], [, b]) => b - a)
+      const top5 = sortedValues.slice(0, 5)
+      socket.emit('game-over', {top: top5})
+      io.to(roomName).emit('game-over', {top: top5});
+    } else {
+      question = questions[currentQIndex].text
+      var answers = questions[currentQIndex].answers;
+      socket.emit('recieve-question', {question, answers})
+      io.to(roomName).emit('recieve-question', {question, answers});
+    }
+  })
+
+  // recieve an answer
+  socket.on('answer', (data) => {
+    // check the question via data.question and check correct answer and do accordingly
+    if(data.answer == questions[currentQIndex].correctAnswer){
+      userPlayerPoints[data.socketId][1]++;
+      // console.log(data.socketId, " :", userPlayerPoints[data.socketId][1])
     }
   })
 
@@ -90,3 +126,115 @@ function generateGamePin(){
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
+
+const questions = [{
+  text: "In Spain, people eat 12 ____ right before midnight. One for each bell strike.",
+  time: 10,
+  answers: [
+      "olives",
+      "tapas",
+      "grapes",
+      "pieces of bread"
+  ],
+  correctAnswer: "grapes"
+},
+{
+  text: "Which country has a giant hour glass wheel that needs to be turned on its head at midnight?",
+  time: 10,
+  answers: [
+      "Hungary",
+      "Romania",
+      "Belgium",
+      "Switzerland"
+  ],
+  correctAnswer: "Hungary"
+},
+{
+  text: "In Belgium, kids prepare ______ in school for their grandparents and godparents.",
+  time: 10,
+  answers: [
+      "small gifts",
+      "party crowns and hats",
+      "songs",
+      "New Year's letters"
+  ],
+  correctAnswer: "New Year's letters"
+},
+{
+  text: "Which country calls New Year's Eve Hogmanay?",
+  time: 10,
+  answers: [
+      "Ireland",
+      "Scotland",
+      "Greenland",
+      "England"
+  ],
+  correctAnswer: "Scotland"
+},
+{
+  text: "People in Finland predict what'll happen in the new year by _______.",
+  time: 10,
+  answers: [
+      "reading tea leaves",
+      "reading palms",
+      "casting molten tin into water and interpreting the shape",
+      "visiting fortune tellers"
+  ],
+  correctAnswer: "casting molten tin into water and interpreting the shape"
+},
+{
+  text: "What is baked into sweets as a good luck token in Bolivia?",
+  time: 10,
+  answers: [
+      "Pomegranate seeds",
+      "Grapes",
+      "Almonds",
+      "Coins"
+  ],
+  correctAnswer: "Coins"
+},
+{
+  text: "In which city in the U.S. do millions of people gather to watch the ball drop at midnight?",
+  time: 10,
+  answers: [
+      "New York City, NY",
+      "Washington, D.C.",
+      "Austin, TX",
+      "Dallas, TX"
+  ],
+  correctAnswer: "New York City, NY"
+},
+{
+  text: "In Russia, people write down wishes on paper. What do they do with them afterwards?",
+  time: 10,
+  answers: [
+      "Put them in a jar and keep it closed for a year.",
+      "Burn them, throw it in a Champagne glass and drink it.",
+      "Burn them in the fire place.",
+      "Tie them to balloons and let them fly away."
+  ],
+  correctAnswer: "Burn them, throw it in a Champagne glass and drink it."
+},
+{
+  text: "People in Colombia believe that _____ will increase their chances to travel in the new year.",
+  time: 10,
+  answers: [
+      "packing their suitcases by midnight",
+      "making a wish on their passports",
+      "buying a new suitcase by midnight",
+      "running around the block with their suitcases"
+  ],
+  correctAnswer: "running around the block with their suitcases"
+},
+{
+  text: "Why do Ecuadorians burn homemade puppets at midnight?",
+  time: 10,
+  answers: [
+      "It's a replacement for fireworks, as those are illegal.",
+      "To burn away the old year and start with a clean slate.",
+      "They believe puppets are evil.",
+      "To protect themselves against spirits."
+  ],
+  correctAnswer: "To burn away the old year and start with a clean slate."
+},
+]
